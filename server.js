51cac1729b4d7798194c03b15901aa77897f416b
goplanit19@gmail.com <goplanit19@gmail.com>
2021-09-15 22:30:21 +0900

    add: git 초기설정 및 Mission 1, 2, 3 진행

---
+++ server.js
@@ -0,0 +1,16 @@
+import express from 'express';
+
+const app = express();
+const PORT = 8000 | process.env.DATABASE_URL;
+
+app.get('/', (req, res) => {
+  res.send('S2팀');
+});
+
+app.listen(PORT, err => {
+  try {
+    console.log(`Server is running on ${PORT}`);
+  } catch {
+    console.log(err);
+  }
+});
